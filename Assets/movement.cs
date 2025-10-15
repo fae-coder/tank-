27985a6c3580883c719b1406744fb63ad4251b59
fae-coder <rxd511@york.ac.uk>
2025-10-15 09:30:20 +0100

    Update movement.cs

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
 
-public class movement : MonoBehaviour
+public class Tankmovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float speed = 50.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update () 
+{
+float throttle = Input.GetAxis ("Vertical");
+float movement = throttle * speed * Time.deltaTime
+
+Transform t = gameObject.transform;
+t.Translate (0.0f, 0.0f, movement);
+}
 }
