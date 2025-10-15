a5d3724d58a3c797d271d62f58b2f4f9c9ce08ab
fae-coder <rxd511@york.ac.uk>
2025-10-15 09:37:07 +0100

    Update movement.cs

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
 
-public class Tankmovement : MonoBehaviour
+public class TankMovement : MonoBehaviour
 {
     public float speed = 50.0f;
 
     void Update () 
 {
 float throttle = Input.GetAxis ("Vertical");
-float movement = throttle * speed * Time.deltaTime
+float movement = throttle * speed * Time.deltaTime;
 
 Transform t = gameObject.transform;
 t.Translate (0.0f, 0.0f, movement);
