6810af293f08a6c39d9c5517fddcf7f80ce59c9b
fae-coder <rxd511@york.ac.uk>
2025-10-15 09:54:27 +0100

    .

--- Assets/movements.cs
+++ Assets/movements.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
 public class TankMovement : MonoBehaviour
+public float turnSpeed = 75.0f;
 {
     public float speed = 50.0f;
 
@@ -8,8 +9,22 @@ public class TankMovement : MonoBehaviour
 {
 float throttle = Input.GetAxis ("Vertical");
 float movement = throttle * speed * Time.deltaTime;
+float rotation = 0.0f;
+{
+    rotation = -turnSpeed * Time.deltaTime;
+}
+    else if (Input.GetKey (Keycode.E))
+{
+    rotation = turnSpeed * Time.deltaTime;
+}
+
+Transform t = gameObject.transform;
 
 Transform t = gameObject.transform;
 t.Translate (0.0f, 0.0f, movement);
 }
 }
+
+
+
+
